0fc5b205b84360cfb249373e2cdc95162662ea0d
wsalame <wsalame@users.noreply.github.com>
2017-03-27 21:16:22 -0400

    217110629 - Scrap a single day (Python) (#10)

---
+++ ScrapperHockey/webapp.py
@@ -0,0 +1,84 @@
+import urllib
+from lxml import html
+from flask import Flask, json
+from flask import request
+from Models import *
+
+app = Flask(__name__)
+day_cache=dict()
+cache_miss=0 #TODO useful for stats
+
+def find_winner(game_element):
+    el = game_element[0].xpath("//tr[@class='winner']/td/a")
+    return el[0].text
+
+def find_loser(game_element):
+    el = game_element[0].xpath("//tr[@class='loser']/td/a")
+    return el[0].text
+
+def find_home(game_element):
+    el = game_element[0].xpath("//tr[position()=2]/td/a")
+    return el[0].text
+
+def find_score_winner(game_element):
+    el = game_element[0].xpath("//tr[@class='winner']/td[@class='right']")
+    return el[0].text
+
+def find_score_loser(game_element):
+    el = game_element[0].xpath("//tr[@class='loser']/td[@class='right']")
+    return el[0].text
+
+def find_is_regulation_time_win(game_element):
+    el = game_element[0].xpath("//tr[position()=2]/td[position()=3]")
+    return not el[0].text.startswith('OT')
+
+def is_in_cache(day, month, year):
+    return ('%d%d%d' % (day, month, year)) in day_cache
+
+def parse_scores_of_the_day(page, day, month, year):
+    print "Retrieving"
+    game_list = []
+    for gameTmp in page.xpath("//table[contains(@class, 'teams')]"):
+        game_element = html.HtmlElement(gameTmp)
+        game = Game()
+
+        game.winner = find_winner(game_element)
+        game.loser = find_loser(game_element)
+        game.home = find_home(game_element)
+        game.score_winner = find_score_winner(game_element)
+        game.score_loser = find_score_loser(game_element)
+        game.is_regulation_time_win = find_is_regulation_time_win(game_element)
+        game_list.append(game)
+
+    dayObj = Day(day, month, year)
+    dayObj.games = game_list
+
+    day_cache[('%d%d%d' % (day, month, year))]=dayObj
+
+    return dayObj
+
+@app.route('/hockey/v1/<int:day>/<int:month>/<int:year>', methods=['GET'])
+def oneDay(day, month, year):
+    global cache_miss
+
+    if is_in_cache(day, month, year):
+        day_obj = day_cache[('%d%d%d' % (day, month, year))]
+    else:
+        url = "http://www.hockey-reference.com/boxscores/index.cgi?day=%d&month=%d&year=%d" % (day, month, year)
+        page = html.fromstring(urllib.urlopen(url).read())
+        day_obj = parse_scores_of_the_day(page, day, month, year)
+        cache_miss += 1
+
+    contains_pretty_print = 'pretty' in request.args
+
+    response = app.response_class(
+        response=json.dumps(day_obj.serialize, indent=(int(request.args.get('pretty')) if contains_pretty_print else None),
+                            sort_keys=True),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='', port=8989)
